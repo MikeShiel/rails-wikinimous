5719bbb1aa74bb9b3ab95cf6394692e8fa47b9db
Mike Shiel <shiel90@gmail.com>
2017-04-18 17:12:15 +0200

    rails-wiki created and edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+
+  # get 'articles/show'
+
+  # get 'articles/new'
+
+  # get 'articles/create'
+
+  # get 'articles/edit'
+
+  # get 'articles/update'
+
+  # get 'articles/destory'
+
+  # get 'index/show'
+
+  # get 'index/new'
+
+  # get 'index/create'
+
+  # get 'index/edit'
+
+  # get 'index/update'
+
+  # get 'index/destory'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
